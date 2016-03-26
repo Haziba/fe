336de8901ccc87bc71fe24d693e8d228d006356c
Harry <harry.boyes@gmail.com>
2016-03-26 15:27:33 +0000

    Checks if user exists before updating them via update

--- server/api/users/update.js
+++ server/api/users/update.js
@@ -1,14 +1,18 @@
 module.exports = function(router, db, models){
-	router.route('/update/:user_id').get(function(req, res){
-		//todo: Check player exists
-		
+	router.route('/update/:user_id').post(function(req, res){
 		var player = req.body;
 		player.id = req.params.user_id;
 		
-		db.set(models.Player, player).then(function(){
-			res.json({"success": true});
-		}, function(err){
-			res.json({"success": false, "error": err});
+		db.getById(models.Player, player.id).then(function(player){
+			if(player != null){
+				db.set(models.Player, player).then(function(){
+					res.json({"success": true, "player": player});
+				}, function(err){
+					res.json({"success": false, "error": err});
+				});
+			} else {
+				res.json({"success": false, "error": "Player does not exist"});
+			}
 		});
 	});
 }
